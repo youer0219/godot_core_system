5ef0c1893d7414b2ced3da796b5c4aad189dd1d9
2410250284@qq.com <2410250284@qq.com>
2025-02-12 12:33:53 +0800

    移除逻辑组件容器EntityLogic

--- source/entity_system/entity_manager.gd
+++ source/entity_system/entity_manager.gd
@@ -49,7 +49,8 @@ func get_entity_scene(entity_id: StringName) -> PackedScene:
 ## [param scene_path] 场景路径
 ## [param load_mode] 加载模式
 ## [return] 加载的实体
-func load_entity(entity_id: StringName, scene_path: String, load_mode: CoreSystem.ResourceManager.LOAD_MODE = CoreSystem.ResourceManager.LOAD_MODE.IMMEDIATE) -> PackedScene:
+func load_entity(entity_id: StringName, scene_path: String, 
+		load_mode: CoreSystem.ResourceManager.LOAD_MODE = CoreSystem.ResourceManager.LOAD_MODE.IMMEDIATE) -> PackedScene:
 	if _entity_resource_cache.has(entity_id):
 		push_warning("实体已存在: %s" % entity_id)
 		return _entity_resource_cache[entity_id]
@@ -78,14 +79,14 @@ func unload_entity(entity_id: StringName) -> void:
 func create_entity(entity_id: StringName, entity_config: Resource, parent : Node = null) -> Node:
 	var instance : Node =  _resource_manager.get_instance(_entity_path_map[entity_id])
 	if not instance:
-		instance = _entity_resource_cache[entity_id].instantiate()
+		instance = get_entity_scene(entity_id).instantiate()
 	
 	if not instance or not instance is Node:
 		push_error("实体实例不是 Node 类型: %s" % entity_id)
 		return
 	
 	if instance.has_method("initialize"):
-		instance.initialize(entity_config)
+		init_entity(instance, entity_config)
 
 	if parent:
 		parent.add_child(instance)
@@ -93,10 +94,47 @@ func create_entity(entity_id: StringName, entity_config: Resource, parent : Node
 	entity_created.emit(entity_id, instance)
 	return instance
 
+## 初始化实体
+## [param entity_id] 实体ID
+## [param instance] 要初始化的实体
+func init_entity(instance : Node, entity_config: Resource) -> void:
+	if not is_entity(instance):
+		push_error("实体不是 Entity 类型: %s" % instance)
+		return
+	
+	if not instance.has_method("initialize"):
+		# push_error("实体没有 initialize 方法: %s" % instance)
+		return
+	for component in instance.components:
+		if is_component(component):
+			initialize_component(component, entity_config)
+	instance.initialize(entity_config)
+
+## 更新实体
+## [param entity_id] 实体ID
+## [param instance] 要更新的实体
+func update_entity(instance : Node, data: Dictionary = {}) -> void:
+	if not is_entity(instance):
+		push_error("实体不是 Entity 类型: %s" % instance)
+		return
+	for component in instance.components:
+		if is_component(component):
+			update_component(component, data)
+	if instance.has_method("update"):
+		instance.update(data)
+
 ## 销毁实体
 ## [param entity_id] 实体ID
 ## [param instance] 要销毁的实体
 func destroy_entity(entity_id: StringName, instance : Node) -> void:
+	if not is_entity(instance):
+		push_error("实体不是 Entity 类型: %s" % instance)
+		return
+	for component in instance.components:
+		if is_component(component):
+			remove_component(instance, component)
+	if instance.has_method("destroy"):
+		instance.destroy()
 	_resource_manager.recycle_instance(_entity_path_map[entity_id], instance)
 	entity_destroyed.emit(entity_id, instance)
 
@@ -104,3 +142,81 @@ func destroy_entity(entity_id: StringName, instance : Node) -> void:
 func clear_entities() -> void:
 	for entity_id in _entity_resource_cache.keys():
 		_resource_manager.clear_instance_pool(_entity_path_map[entity_id])
+
+## 是否为实体
+static func is_entity(node : Node) -> bool:
+	if "components" in node and node.components is Dictionary:
+		return true
+	return false
+
+## 添加组件
+## [param component_id] 组件ID
+## [param component] 要添加的组件
+func add_component(entity: Node, component: Object) -> bool:
+	if not is_component(component):
+		push_error("不是合法组件: %s, 无法添加" % component)
+		return false
+	if not is_entity(entity):
+		push_error("不是合法实体: %s, 无法添加组件" % entity)
+		return false
+	entity.components[component.component_id] = component
+	return true
+
+## 获取组件
+## [param entity] 实体
+## [param component_id] 组件ID
+## [return] 组件实例
+func get_component(entity: Node, component_id: StringName) -> Object:
+	if not is_entity(entity):
+		push_error("不是合法实体: %s, 无法获取组件" % entity)
+		return null
+	return entity.components.get(component_id)
+
+## 初始化组件
+## [param component] 组件
+## [param data] 初始化数据
+func initialize_component(component: Object, entity_config: Resource) -> void:
+	if not is_component(component):
+		push_error("不是合法组件: %s, 无法初始化" % component)
+		return
+	if not component.has_method("initialize"):
+		# push_error("组件 %s 没有 initialize 方法" % component)
+		return
+	component.initialize(entity_config)
+
+## 更新组件
+## [param component] 组件
+## [param data] 更新数据
+func update_component(component: Object, data: Dictionary = {}) -> void:
+	if not is_component(component):
+		push_error("不是合法组件: %s, 无法更新" % component)
+		return
+	if not component.has_method("update_data"):
+		# push_error("组件 %s 没有 update_data 方法" % component)
+		return
+	component.update_data(data)
+
+## 移除组件
+## [param entity] 实体
+## [param component_id] 组件ID
+func remove_component(entity: Node, component_id: StringName) -> void:
+	if not is_entity(entity):
+		push_error("不是合法实体: %s, 无法移除组件" % entity)
+		return
+	var component : Object = get_component(entity, component_id)
+	if not is_component(component):
+		push_error("不是合法组件: %s, 无法移除" % component)
+		return
+	if not component.has_method("cleanup"):
+		# push_error("组件 %s 没有 cleanup 方法" % component)
+		return
+	component.cleanup()
+	entity.components.erase(component_id)
+
+## 是否是组件
+## [param node] 节点
+## [return] 是否是组件
+static func is_component(node : Object) -> bool:
+	if "component_id" in node and node.component_id is StringName:
+		return true
+	return false
